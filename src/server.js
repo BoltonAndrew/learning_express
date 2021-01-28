282f99dfdb810dbdc5c8958b630acc2ed0d75fa8
BostonWafro <wafrogaming@gmail.com>
2021-01-28 12:38:04 +0000

    added basic POST functions

--- src/server.js
+++ src/server.js
@@ -2,13 +2,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("hello world");
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send({ data: req.body });
 });
 
 app.get("/data", (req, res) => {
     console.log(req.query);
     res.send({ name: req.query.name });
+});
+
+app.post("/data", (req, res) => {
+    res.send({ data: req.body });
 });
 
 app.get("/stuff/:id", (req, res) => {
@@ -18,6 +29,10 @@ app.get("/stuff/:id", (req, res) => {
 
 app.get("/stuff", (req, res) => {
     res.send("You have found all the stuff");
+});
+
+app.post("/stuff", (req, res) => {
+    res.send({ data: req.body });
 });
 
 app.get("/person/:id", (req, res) => {
